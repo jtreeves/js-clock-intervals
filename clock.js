c5936726d9c391bc85d6dcf230fa9fe32c5ff1ec
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-10-23 14:35:15 -0400

    [app] updated function.

--- clock.js
+++ clock.js
@@ -2,10 +2,22 @@ const hourHand = document.querySelector("#hour");
 const minuteHand = document.querySelector("#minute");
 const secondHand = document.querySelector("#second");
 
+const numbers = [30, 6];
 
-function secondRotation() {
-      let degRotation = secondHand.setAttribute("transform", "rotate(100deg)");
+
+function rotation(increment) {
+      let degRotation = secondHand.style.transform = `rotate(${increment}deg)`;
       return degRotation;
 }
 
-secondRotation();
+// setInterval(rotation, 1000, 100)
+
+function secondRotation() {
+    let incrmnt = 0;
+    for(let i = 0; i < 60; i++) {
+        incrmnt += numbers[1];
+    }
+    rotation(incrmnt);
+}
+
+setInterval(secondRotation, 1000);
